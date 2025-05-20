efd6acff6e3b875129e604164b946fd9b8f3fba2
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-05-20 23:32:05 +0600

    DB Connected

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42yqa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const usersCollection = client.db('iaDB').collection('users');
+    const contactsCollection = client.db('iaDB').collection('contacts');
+
+    // GET ALL USERS
+    app.get('/users', async(req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    })
+
+    // GET ALL CONTACTS
+    app.get('/contacts', async(req, res) => {
+        const result = await contactsCollection.find().toArray();
+        res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("International Affairs is Waiting");
+});
+
+app.listen(port, () => {
+  console.log(`IA is Waiting on port ${port}`);
+});
