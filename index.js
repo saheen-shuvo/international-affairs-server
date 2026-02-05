1d1a4b04e89c103d89fffe0a7dfe46be6b06666d
Saheen Alam Shuvo <saheenalamshuvo@Saheens-MacBook-Air.local>
2026-02-05 13:17:43 +0600

    Changed Port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // Middleware
